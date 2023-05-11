b63967fd2ead4b99a325d4077dded6ca00c9816c
denis-ttk-1975 <denis.ttk@gmail.com>
2023-05-11 12:36:44 +0300

    cors in backend fixed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      credentials: true,
+      origin: '*',
+    }),
+  );
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
