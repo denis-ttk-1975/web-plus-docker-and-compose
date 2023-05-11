540c85e985f92a666249756d7f6e0ed20ddad7c0
denis-ttk-1975 <denis.ttk@gmail.com>
2023-05-11 13:17:41 +0300

    cors enabled via app.enableCors();

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   // app.use(
   //   cors({
   //     credentials: true,
